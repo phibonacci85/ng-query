96c5ccf7d2a754f0fdfa52aa5d62e1d055cff968
John Baker <phibonacci85@gmail.com>
2018-03-26 00:25:37 -0500

    angular 4 animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +8,51 @@ import { animate, style, transition, trigger } from '@angular/animations';
   animations: [
     trigger('panelState', [
       transition(':enter', [
-        style({
-          opacity: 0,
-          transform: 'translateX(100%)'
-        }),
-        animate(200)
+        group([
+          query(':self', [
+            style({
+              opacity: 0
+            }),
+            animate(1000)
+          ]),
+          query('.panel-body', [
+            style({
+              transform: 'translateX(-100%)',
+              opacity: 0
+            }),
+            animate(300)
+          ]),
+          query('.panel-footer', [
+            style({
+              transform: 'translateY(300px)',
+              opacity: 0
+            }),
+            animate(300)
+          ])
+        ])
       ]),
       transition(':leave', [
         animate(200, style({
           transform: 'translateX(-100%)',
           opacity: 0
         }))
+      ]),
+      transition('* => *', [
+        query(':enter', [
+          style({
+            transform: 'scale(1)'
+          }),
+          animate(200, style({
+            transform: 'scale(1.1)'
+          })),
+          animate(100)
+        ], {optional: true}),
+        query('div p, button', [
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(200)
+        ])
       ])
     ])
   ]
